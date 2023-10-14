72fbf39ea01f9ce9fddaa736a67f6d384043c151
behzad dara <Behzad.dara.99@gmail.com>
2023-10-14 17:00:09 +0330

    Add project files.

---
+++ PatternChecking/Order.cs
@@ -0,0 +1,60 @@
+﻿namespace PatternChecking
+{
+    public class Order
+    {
+        public Customer Customer { get; set; } = new Customer();
+        public decimal TotalCost { get; set; }
+
+        public decimal Tax1()
+        {
+            if (Customer.IsPremium && Customer.Age < 20)
+            {
+                if (TotalCost < 100)
+                    return 0;
+                else
+                    return 5;
+            }
+            else if (Customer.IsPremium)
+            {
+                if (TotalCost < 100)
+                    return 10;
+                else
+                    return 15;
+            }
+            else if (Customer.Age < 20)
+            {
+                if (TotalCost < 100)
+                    return 20;
+                else
+                    return 25;
+            }
+            else
+            {
+                if (TotalCost > 100)
+                    return 30;
+                else
+                    return 35;
+            }
+        }
+
+        public decimal Tax2() => this switch
+        {
+            { Customer: { IsPremium: true, Age: < 20 }, TotalCost: var cost } when cost < 100 => 0 ,
+            { Customer: { IsPremium: true, Age: < 20 } } => 5 ,
+            { Customer.IsPremium: true, TotalCost: var cost } when cost < 100 => 10,
+            { Customer.IsPremium: true } => 15,
+            { Customer.Age: < 20, TotalCost: var cost } when cost < 100 => 20,
+            { Customer.Age: < 20 } => 25,
+            { TotalCost: > 100 } => 30,
+            _ => 35
+        };
+
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsPremium { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ PatternChecking/Program.cs
@@ -0,0 +1,100 @@
+﻿using PatternChecking;
+
+var testCases = new List<Order>
+{
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x1",
+            IsPremium = true,
+            Age = 18
+        },
+        TotalCost = 50
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x2",
+            IsPremium = true,
+            Age = 18
+        },
+        TotalCost = 150
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x3",
+            IsPremium = true,
+            Age = 22
+        },
+        TotalCost = 50
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x4",
+            IsPremium = true,
+            Age = 22
+        },
+        TotalCost = 150
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x5",
+            IsPremium = false,
+            Age = 18
+        },
+        TotalCost = 50
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x6",
+            IsPremium = false,
+            Age = 18
+        },
+        TotalCost = 150
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x7",
+            IsPremium = false,
+            Age = 22
+        },
+        TotalCost = 50
+    },
+    new Order
+    {
+        Customer = new Customer
+        {
+            Name = "x8",
+            IsPremium = false,
+            Age = 22
+        },
+        TotalCost = 150
+    },
+};
+
+foreach (var testCase in testCases)
+{
+    Console.Write($"IsPremium: {testCase.Customer.IsPremium}, Age: {testCase.Customer.Age}, TotalCost: {testCase.TotalCost}, ");
+    var tax1 = testCase.Tax1();
+    var tax2 = testCase.Tax2();
+    if (tax1 == tax2)
+    {
+        Console.WriteLine($"tax: {tax1}");
+    }
+    else
+    {
+        Console.WriteLine($"tax1: {tax1}, tax2 is {tax2}");
+    }
+}
